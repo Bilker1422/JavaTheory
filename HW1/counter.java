e538b75cc4db6914a50d0f6d44c795e211f0d701
Bilker <Yahyaksa28@gmail.com>
2021-10-03 03:34:55 +0300

    Add HW1

---
+++ HW1/counter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class counter
+{
+    public static void main(String[] args)
+    {
+        String text;
+        Scanner sc = new Scanner(System.in);
+        
+        text =  sc.nextLine();
+
+    
+    }
+}
