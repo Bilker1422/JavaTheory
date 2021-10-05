9ce5d678bd726cd930d99bd715e04ae42d9ec248
Bilker <Yahyaksa28@gmail.com>
2021-10-05 14:12:57 +0300

    Added HW2 to v2

---
+++ HW2/array.java
@@ -0,0 +1,18 @@
+public class array
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = new int[12];
+        int a = 1, b = 0 , total;
+        for (int i = 0 ; i < 12; i++)
+        {
+            total = a + b;
+            b = a;
+            a = total;
+            numbers[i] = total;
+            System.out.printf("%d ", numbers[i]);
+
+
+        }
+    }
+}
---
+++ HW2/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+
+    public static void main(String[] args)
+    {
+        int[] numbers = new int[12];
+        int b =1;
