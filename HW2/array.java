5146cadf31daa6de52b41d9561126bd5cf1e4a73
Bilker <Yahyaksa28@gmail.com>
2021-10-09 16:21:00 +0300

    fix

--- HW2/array.java
+++ HW2/array.java
@@ -2,18 +2,19 @@ public class array
 {
     public static void main(String[] args)
     {
-        int[] numbers = new int[12];
+        int[] numbers = new int[10];
         int a = 1, b = 0 , total;
-        for (int i = 0 ; i < 12; i++)
+        for (int i = 0 ; i < 10; i++)
         {
             total = a + b;
-            b = a;
-            a = total;
+            a = b;
+            b = total;
             numbers[i] = total;
+            System.out.printf("%d,", numbers[i]);
+
         }
-        for(int i = 0; i < 12 ; i++)
+        for(int i = 0; i < 10 ; i++)
         {
-            System.out.printf("%d, ", numbers[i]);
         }
     }
 }
